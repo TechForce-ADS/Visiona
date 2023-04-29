ae29623b1c9bda9dae8d4f63a9ad6b9179f980a4
Pedro Seraggi <projetodispvag@gmail.com>
2023-04-29 14:03:34 -0300

    Update database.js

--- Sprints/3_Sprint/codigo/src/config/database.js
+++ Sprints/3_Sprint/codigo/src/config/database.js
@@ -1,7 +1,7 @@
 module.exports = {
     username: "postgres",
-    password: "postgres",
-    database: "postgres",
+    password: "seraggi123",
+    database: "users",
     host: "127.0.0.1",
     dialect: "postgres",
 };
