f1ebe495e196f3dd0f8beb163be05dc16f6baf48
techforce <projetodispvag@gmail.com>
2023-05-29 22:27:22 -0300

    Update profile.tsx

--- Sprints/3_Sprint/codigo/src/components/profile/profile.tsx
+++ Sprints/3_Sprint/codigo/src/components/profile/profile.tsx
@@ -3,12 +3,34 @@ import '../../App.css'
 import Navbar from '../navbar/navbarComum';
 
 
-const Profile: React.FC = () => {
-  return (
-    <>
-    <Navbar />
-    </>
-  )  
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+interface InformacoesLoginProps {
+  nomeUsuario: string;
+  emailUsuario: string;
 }
+
+const InformacoesLogin: React.FC<InformacoesLoginProps> = ({
+  nomeUsuario,
+  emailUsuario,
+}) => {
+  return (
+    <div>
+      <h1>Informações de Login</h1>
+      <p>Nome: {nomeUsuario}</p>
+      <p>Email: {emailUsuario}</p>
+    </div>
+  );
+};
+
+// Definir as informações de login
+const nomeUsuario = 'Exemplo';
+const emailUsuario = 'exemplo@email.com';
+
+ReactDOM.render(
+  <InformacoesLogin nomeUsuario={nomeUsuario} emailUsuario={emailUsuario} />,
+  document.getElementById('root')
+);
 
 export default Profile;
