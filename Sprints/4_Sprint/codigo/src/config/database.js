cba4167c2cd2d6df5fe1888b5a465575d998625b
Pedro Seraggi <projetodispvag@gmail.com>
2023-06-07 00:16:46 -0300

    Final

--- Sprints/4_Sprint/codigo/src/config/database.js
+++ Sprints/4_Sprint/codigo/src/config/database.js
@@ -1,6 +1,6 @@
 module.exports = {
     username: "postgres",
-    password: "postgres",
+    password: "seraggi123",
     database: "users",
     host: "127.0.0.1",
     dialect: "postgres",
--- Sprints/4_Sprint/codigo/src/server.js
+++ Sprints/4_Sprint/codigo/src/server.js
@@ -11,6 +11,10 @@ app.use(express.json());
 app.use(routes);
 
 
+
+
+
+
 app.listen(3333, () => console.log("Server is on"))
 
 module.exports = App;
