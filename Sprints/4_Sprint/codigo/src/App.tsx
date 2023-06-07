d3e509435df0d43fae820f66343e0bc3c74cd125
Pedro Seraggi <projetodispvag@gmail.com>
2023-06-06 23:47:33 -0300

    build

--- Sprints/4_Sprint/codigo/src/App.tsx
+++ Sprints/4_Sprint/codigo/src/App.tsx
@@ -1,10 +1,4 @@
 import React from 'react';
-//import Login from './components/login/login';
-//import Contact from './components/contact/contato';
-//import Dashboard from './components/dashboard/dashboard';
-//import Profile from './components/profile/profile';
-//import List from './components/list/list';
-
 
 
 const App: React.FC = () => {
